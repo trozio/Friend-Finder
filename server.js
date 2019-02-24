8c3a88c4e66cb5e319b61eb832069f511c929db6
trozio <daniel.l.adelman@gmail.com>
2019-02-23 22:45:48 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let express = require("express");
 let app = express();
 
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
